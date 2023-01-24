c1885e53e3fa77771a988fa8103660b8632dd940
XXXXX <abc@gmail.com>
2023-01-24 09:49:51 +0900

    new app file

---
+++ 20230123/app.js
@@ -0,0 +1,21 @@
+// variable declaration
+let a = 10;
+let b = a + 20;
+
+console.log(a);
+console.log(b);
+
+a + a + b;
+
+// ++
+a++;
+console.log(a);
+
+console.log(++a);
+
+let c = 0;
+let d = 0;
+
+let e = (--c) + (--d) + (c++) + (d++);
+
+console.log(e);
